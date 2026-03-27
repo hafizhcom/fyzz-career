ea0bb968ff545c66f722c8d229a1606f385fa24c
Fyzz20 <nurhayatiti1122@gmail.com>
2026-03-27 10:28:22 +0700

    Update toko.js

--- toko.js
+++ toko.js
@@ -19,7 +19,7 @@ document.getElementById("contactForm").addEventListener("submit", function(e) {
     var url = "https://wa.me/" + nomor + 
             "?text=Halo,%20saya%20" + nama + 
             "%0ALayanan%20Saya:%20" + layanan +
-            "%0APesan:%20" + pesan;
+            "%0ADetail:%20" + pesan;
 
     window.open(url, "_blank");
 });
